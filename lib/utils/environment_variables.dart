f821c03df119d477ccc32ff1345276fd11b0d4db
Chinmay Shringi <31031919+ChinmayShringi@users.noreply.github.com>
2021-06-08 12:18:50 +0530

    Update environment_variables.dart

--- lib/utils/environment_variables.dart
+++ lib/utils/environment_variables.dart
@@ -1,73 +1,22 @@
-import 'dart:io';
-import 'dart:math';
 import 'package:flutter/material.dart';
-// import 'package:image/image.dart' as Im;
-import 'package:image_picker/image_picker.dart';
-import 'package:intl/intl.dart';
-import 'package:meta/meta.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:skype_clone/enum/user_state.dart';
 
-class Utils {
-  static String getUsername(String email) {
-    return "live:${email.split('@')[0]}";
-  }
+class UniversalVariables {
+  static final Color blueColor = Color(0xff2b9ed4);
+  static final Color blackColor = Color(0xff19191b);
+  static final Color greyColor = Color(0xff8f8f8f);
+  static final Color userCircleBackground = Color(0xff2b2b33);
+  static final Color onlineDotColor = Color(0xff46dc64);
+  static final Color lightBlueColor = Color(0xff0077d7);
+  static final Color separatorColor = Color(0xff272c35);
 
-  static String getInitials(String name) {
-    List<String> nameSplit = name.split(" ");
-    String firstNameInitial = nameSplit[0][0];
-    String lastNameInitial = nameSplit[1][0];
-    return firstNameInitial + lastNameInitial;
-  }
+  static final Color gradientColorStart = Color(0xff00b6f3);
+  static final Color gradientColorEnd = Color(0xff0184dc);
 
-  // this is new
+  static final Color senderColor = Color(0xff2b343b);
+  static final Color receiverColor = Color(0xff1e2225);
 
-  static Future<File> pickImage({@required ImageSource source}) async {
-    File selectedImage = await ImagePicker.pickImage(source: source);
-    return await compressImage(selectedImage);
-  }
-
-  static Future<File> compressImage(File imageToCompress) async {
-    final tempDir = await getTemporaryDirectory();
-    final path = tempDir.path;
-    int rand = Random().nextInt(10000);
-
-    // Im.Image image = Im.decodeImage(imageToCompress.readAsBytesSync());
-    // Im.copyResize(image, width: 500, height: 500);
-
-    // return new File('$path/img_$rand.jpg')
-    //   ..writeAsBytesSync(Im.encodeJpg(image, quality: 85));
-  }
-
-  static int stateToNum(UserState userState) {
-    switch (userState) {
-      case UserState.Offline:
-        return 0;
-
-      case UserState.Online:
-        return 1;
-
-      default:
-        return 2;
-    }
-  }
-
-  static UserState numToState(int number) {
-    switch (number) {
-      case 0:
-        return UserState.Offline;
-
-      case 1:
-        return UserState.Online;
-
-      default:
-        return UserState.Waiting;
-    }
-  }
-
-  static String formatDateString(String dateString) {
-    DateTime dateTime = DateTime.parse(dateString);
-    var formatter = DateFormat('dd/MM/yy');
-    return formatter.format(dateTime);
-  }
+  static final Gradient fabGradient = LinearGradient(
+      colors: [gradientColorStart, gradientColorEnd],
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight);
 }
